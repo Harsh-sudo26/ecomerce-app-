be3ad5b17b639718c8c514f25aea6035e49692f1
Harsh <animeart711@gmail.com>
2025-07-21 20:52:03 +0530

    signup

--- lib/signup/sign_up.dart
+++ lib/signup/sign_up.dart
@@ -6,7 +6,22 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Sign Up'),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
