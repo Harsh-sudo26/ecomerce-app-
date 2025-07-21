5b6965fbccb5a5baed4a6b8b60cf67a61a9f849f
Harsh <animeart711@gmail.com>
2025-07-21 20:45:59 +0530

    login completed

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/images_const/image_const.dart';
+import 'package:ecommerce/signup/sign_up.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,65 +8,95 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size(0, 100),
-        child: Container(
-          height: 100,
-          color: Colors.amber,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 27),
-              child: Text(
-                'Home Page',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-        ),
-      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Image.asset(loginimg), //image here
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Back !',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Column(
+                    children: [
+                      Text(
+                        'Create Account',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.email, color: Colors.black),
+                hint: Text('Enter Email', style: TextStyle()),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.password_outlined, color: Colors.black),
+                hint: Text('Enter Password', style: TextStyle()),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Hs Cart',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.left,
+                Text('Don\'t have an account?', style: TextStyle(fontSize: 15)),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUp()),
+                    );
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-                // Text(),
               ],
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.email),
-                ),
-                labelText: 'Enter your name',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  icon: Icon(Icons.password),
-                ),
-                labelText: 'Enter your name',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
             ),
           ],
         ),
---
+++ lib/images_const/image_const.dart
@@ -0,0 +1,1 @@
+final String loginimg = "assets/loginui.png";
